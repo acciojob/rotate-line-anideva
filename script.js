7eb702a48568da314297d76de953de640c6efaf5
Aniketh Roy Chowdhury <95467658+anideva@users.noreply.github.com>
2026-02-11 13:08:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 const line= document.getElementById("line");
 let angle=0;
 setInterval(()=>{
-	angle +2;
+	angle +=2;
 	line.style.transform =`
-	translate(-50%, -50%) rotate(${angle}deg)
-	`;
-})
+	translate(-50%, -50%) rotate(${angle}deg)`;
+},20);
